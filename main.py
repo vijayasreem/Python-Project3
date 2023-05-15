5a9206a9ac4a9297c94f62292c9707d0e291185a
Befector <befector@proton.me>
2023-05-15 20:01:11 +0200

    Implemented JSON functionality.

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 # main.py
 
+import sb_conf as conf
+import sb_dbm as db
+
+
+configPath = "Config.json"
+defaultConfig = {
+    "host": "localhost",
+    "user" : "root",
+    "password" : "",
+    "database" : "simplebank" }
+
 if __name__ == '__main__':
-    pass
+    Config = conf.Read(configPath, defaultConfig)
+    Database = db.CreateConnection(
+        Config['host',
+               Config['user',
+                      Config['password'],Config['database']]])
+
+    print(Config)
--- sb_dbm.py
+++ sb_dbm.py
@@ -1,1 +1,11 @@
 # sb_dbm.py
+import pymysql.cursors
+
+# Database Manager
+
+
+def CreateConnection(Host="localhost",
+                     Username="root",
+                     Password="",
+                     Database="simplebank"):
+    pass
